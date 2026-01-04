1db463abfe6153286a146ad5ccd8ecb741840b95
Lucas Gallone <lucasgallone@outlook.fr>
2026-01-04 19:48:43 +0100

    v1.2 update

--- RDSExpert-Plugin.js
+++ RDSExpert-Plugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'RDSExpert',
-    version: '1.1',
+    version: '1.2',
     author: 'Lucas Gallone',
     frontEndPath: 'RDSExpert/RDSExpert-Frontend.js'
 }
@@ -11,5 +11,4 @@ var pluginConfig = {
 // Don't change anything below here if you are making your own plugin
 module.exports = {
     pluginConfig
-
 }
